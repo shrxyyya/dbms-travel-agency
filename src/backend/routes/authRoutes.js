9992c04806ac9f777fb181b12145cffbc330f2c5
shrxyyya <shreya5.mittal3@gmail.com>
2024-10-09 11:34:54 +0530

    minor changes

--- src/backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { login } = require('../controllers/authController');
-const router = express.Router();
-
-// Login route
-router.post('/login', login);
-
-module.exports = router;
